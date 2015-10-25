1edfb80a4e7fe6d151d080d63120beca6f6839e0
Raffael Tancman <rtancman@gmail.com>
2015-10-25 21:44:39 -0200

    add banner dotfiles, fix script main fedora

--- main.sh
+++ main.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 . sh/common/init.sh ;
-
-echo -e "${bakgrn} dotfiles rtancman${txtrst}";
+echo -e "\n\n\n\n";
+echo -e "          888          888     .d888 d8b 888";
+echo -e "          888          888    d88P   Y8P 888";
+echo -e "          888          888    888        888";
+echo -e "      .d88888  .d88b.  888888 888888 888 888  .d88b.  .d8888b";
+echo -e "     d88  888 d88  88b 888    888    888 888 d8P  Y8b 88K";
+echo -e "     888  888 888  888 888    888    888 888 88888888 Y8888b.";
+echo -e "     Y88b 888 Y88..88P Y88b.  888    888 888 Y8b.          X88";
+echo -e "       Y88888   Y88P     Y888 888    888 888   Y8888   88888P'";
+echo -e "                                                             ${bakblu}rtancman${txtrst}";
 echo -e "\n${BGreen} Start...${txtrst}";
 
 if [ "root" != "$USER" ]; then
--- sh/fedora/main.sh
+++ sh/fedora/main.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 dnf update -y;
-# . sh/fedora/scripts/git.sh ;
-# . sh/fedora/scripts/sublimetext3.sh ;
-# . sh/fedora/scripts/docker.sh ;
-# . sh/fedora/scripts/pip.sh ;
-# . sh/fedora/scripts/docker-compose.sh ;
-# . sh/fedora/scripts/skype.sh ;
-# . sh/fedora/scripts/chrome.sh ;
-# . sh/fedora/scripts/virtualbox.sh ;
+
+. sh/fedora/scripts/git.sh ;
+. sh/fedora/scripts/sublimetext3.sh ;
+. sh/fedora/scripts/skype.sh ;
+. sh/fedora/scripts/chrome.sh ;
+. sh/fedora/scripts/virtualbox.sh ;
+. sh/fedora/scripts/docker.sh ;
+. sh/fedora/scripts/pip.sh ;
+. sh/fedora/scripts/docker-compose.sh ;
 # . sh/fedora/scripts/spotify.sh ;
 
 # dnf install python-dnf-plugins-extras-migrate && dnf-2 migrate ;
