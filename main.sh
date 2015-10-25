31ff9212907d1f761ef866858bcc35deddbd473e
Raffael Tancman <rtancman@gmail.com>
2015-10-25 15:46:27 -0200

    fix message when start sh

--- main.sh
+++ main.sh
@@ -3,22 +3,27 @@
 . sh/common/init.sh ;
 
 echo -e "${bakgrn} dotfiles rtancman${txtrst}";
-echo -e "\n${BGreen} Start...";
-echo -e "${txtrst}" ;
+echo -e "\n${BGreen} Start...${txtrst}";
 
 if [ "root" != "$USER" ]; then
-        echo "You must run with root"
-	exit 1
-fi
+	
+	echo -e "${bakred} [Error] You must run with root! ${txtrst}";
 
-osrelease="$(cat /etc/*-release)"
-
-if [[ ${osrelease} =~ 'Fedora' ]]; then
-       . sh/fedora/main.sh;
-elif [[ ${osrelease} =~ 'Debian' ]]; then
-        echo "Debian ;)" ;
 else
-        echo "OS notfound :( send me message or contribute with my project" ;
+
+	osrelease="$(cat /etc/*-release)"
+
+	if [[ ${osrelease} =~ 'Fedora' ]]; then
+	       . sh/fedora/main.sh;
+	elif [[ ${osrelease} =~ 'Debian' ]]; then
+	        echo "Debian ;)" ;
+	else
+	        echo "OS notfound :( send me message or contribute with my project" ;
+	fi
+
 fi
+
+echo -e "${BGreen} Finish...${txtrst}";
+echo -e "\n${bakblu} thx for coming and help me with our project in https://github.com/rtancman/dotfiles${txtrst}";
 
 exit 1
