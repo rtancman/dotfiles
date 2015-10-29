d39d3955fa7167dbd581ec9834ac6135a63a6eec
Raffael Tancman <rtancman@gmail.com>
2015-10-29 14:12:04 -0200

    init java

---
+++ sh/fedora/scripts/javajdk.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo -e "${bakcyn}[Java JDK] Start Install ${txtrst}";
+
+# http://www.if-not-true-then-false.com/2010/install-sun-oracle-java-jdk-jre-7-on-fedora-centos-red-hat-rhel/
+
+# wget http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.rpm ;
+# dnf install -y jdk-7u79-linux-x64.rpm ;
+# rm -rf jdk-7u79-linux-x64.rpm ;
+
+echo -e "${bakgrn}[Java JDK] Finish Install ${txtrst}";
