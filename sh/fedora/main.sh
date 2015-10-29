edcc4d092efaf16713423e783f50c4ad83a8c024
Raffael Tancman <rtancman@gmail.com>
2015-10-29 14:01:18 -0200

    init phpstorm script

--- sh/fedora/main.sh
+++ sh/fedora/main.sh
@@ -2,16 +2,17 @@
 
 # dnf update -y;
 
-# . sh/fedora/scripts/git.sh ;
-# . sh/fedora/scripts/sublimetext3.sh ;
-# . sh/fedora/scripts/skype.sh ;
-# . sh/fedora/scripts/chrome.sh ;
-# . sh/fedora/scripts/virtualbox.sh ;
-# . sh/fedora/scripts/docker.sh ;
-#. sh/fedora/scripts/pip.sh ;
-# . sh/fedora/scripts/docker-compose.sh ;
+. sh/fedora/scripts/git.sh ;
+. sh/fedora/scripts/sublimetext3.sh ;
+. sh/fedora/scripts/skype.sh ;
+. sh/fedora/scripts/chrome.sh ;
+. sh/fedora/scripts/virtualbox.sh ;
+. sh/fedora/scripts/docker.sh ;
+. sh/fedora/scripts/pip.sh ;
+. sh/fedora/scripts/docker-compose.sh ;
 # . sh/fedora/scripts/spotify.sh ;
 . sh/fedora/scripts/slack.sh ;
+# . sh/fedora/scripts/phpstorm.sh ;
 
 # dnf install python-dnf-plugins-extras-migrate && dnf-2 migrate ;
 
