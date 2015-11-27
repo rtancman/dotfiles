dc0c482f8b40e38f986d912b0ed7610ea2f131f7
Raffael Tancman <rtancman@gmail.com>
2015-11-27 17:45:23 -0200

    remove scripts because this branch only install docker

--- sh/debian/scripts/sample.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo -e "${bakcyn}[sample tes] Start Install ${txtrst}";
-
-echo -e "${bakgrn}[sample tes] Finish Install ${txtrst}";
-
--- sh/fedora/scripts/sublimetext3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo -e "${bakcyn}[Sublime Text 3] Start Install ${txtrst}";
-
-curl -L git.io/vWa6H | sh
-
-echo -e "${bakgrn}[Sublime Text 3] Finish Install ${txtrst}";
