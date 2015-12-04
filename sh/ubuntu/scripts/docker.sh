6d195022781fee7afc3fa3221e99d1fb175407f7
Raffael Tancman <rtancman@gmail.com>
2015-12-04 14:15:24 -0200

    add ubuntu versions

--- sh/ubuntu/scripts/docker.sh
+++ sh/ubuntu/scripts/docker.sh
@@ -10,8 +10,21 @@ else
 
 	echo -e "${bakcyn}[Docker] Start Install ${txtrst}";
     
+
+
+
     apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D ;
-    echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" | tee /etc/apt/sources.list.d/docker.list ;
+    if [[ ${osrelease} =~ '15.10' ]]; then
+        echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" | tee /etc/apt/sources.list.d/docker.list ;
+    elif [[ ${osrelease} =~ '15.04' ]]; then
+        echo "deb https://apt.dockerproject.org/repo ubuntu-vivid main" | tee /etc/apt/sources.list.d/docker.list ;
+    elif [[ ${osrelease} =~ '14.04' ]]; then
+        echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | tee /etc/apt/sources.list.d/docker.list ;
+    elif [[ ${osrelease} =~ '12.04' ]]; then
+        echo "deb https://apt.dockerproject.org/repo ubuntu-precise main" | tee /etc/apt/sources.list.d/docker.list ;
+    else
+        echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | tee /etc/apt/sources.list.d/docker.list ;
+    fi
 	apt-get update ;
     apt-get purge lxc-docker ;
     apt-cache policy docker-engine ;
