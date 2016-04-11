7509e1457950e75f59b9c9a589da0912ca17835a
Raffael Tancman <raffael.tancman@m4u.com.br>
2016-04-11 16:14:30 -0300

    add git in prompt

---
+++ sh/common/bashrc/main.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo -e "${bakcyn}[common][bashrc] Start Install ${txtrst}";
+
+. sh/common/heroku/scripts/git-branch-prompt.sh ;
+
+echo -e "${bakcyn}[common][bashrc] Finish Install ${txtrst}";
