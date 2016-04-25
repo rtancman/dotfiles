6a2180bd79ce352176e35fb6607af919abffb126
Raffael Tancman <raffael.tancman@m4u.com.br>
2016-04-25 11:50:18 -0300

    add nscd

--- sh/fedora/scripts/tools.sh
+++ sh/fedora/scripts/tools.sh
@@ -2,6 +2,6 @@
 
 echo -e "${bakcyn}[Tools] Start Install ${txtrst}";
 
-dnf install -y zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel vim-enhanced binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-PAE-devel dkms
+dnf install -y zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel vim-enhanced binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-PAE-devel dkms nscd
 
 echo -e "${bakgrn}[Tools] Finish Install ${txtrst}";
