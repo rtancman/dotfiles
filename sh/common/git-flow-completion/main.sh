b37700f105df4a0461a715afb380db48350a611a
Raffael Tancman <rtancman@gmail.com>
2016-05-31 21:35:03 -0300

    fix telegram and git-flow-completation

--- sh/common/git-flow-completion/main.sh
+++ sh/common/git-flow-completion/main.sh
@@ -8,9 +8,10 @@ else
 
     echo -e "${bakcyn}[common][git-flow-completion] Start Install ${txtrst}";
 
-    cp sh/common/git-flow-completion/scripts/.git-flow-completion /home/$DOTFILE_DEFAULT_USER/.git-flow-completion
-    chmod $DOTFILE_DEFAULT_USER:$DOTFILE_DEFAULT_USER /home/$DOTFILE_DEFAULT_USER/.git-flow-completion
-    echo -e "source ~/git-flow-completion.bash" >> /home/$DOTFILE_DEFAULT_USER/.bashrc
+    cp ./sh/common/git-flow-completion/scripts/.git-flow-completion /home/$DOTFILE_DEFAULT_USER/.git-flow-completion
+    chown $DOTFILE_DEFAULT_USER:$DOTFILE_DEFAULT_USER /home/$DOTFILE_DEFAULT_USER/.git-flow-completion
+    echo -e "# git-flow-completion" >> /home/$DOTFILE_DEFAULT_USER/.bashrc
+    echo -e "source ~/.git-flow-completion" >> /home/$DOTFILE_DEFAULT_USER/.bashrc
 
     echo -e "${bakcyn}[common][git-flow-completion] Finish Install ${txtrst}";
 
