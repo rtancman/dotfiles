de9f603ed6c8f06b7f6337212bc2c0634b28bdad
Raffael Tancman <raffael.tancman@m4u.com.br>
2016-11-16 16:43:26 -0200

    update fedora virtualbox

--- sh/fedora/scripts/virtualbox.sh
+++ sh/fedora/scripts/virtualbox.sh
@@ -10,12 +10,12 @@ else
 
 	echo -e "${bakcyn}[Virtual Box 5.0] Start Install ${txtrst}";
 
-	wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo ;
+	wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
 	mv virtualbox.repo /etc/yum.repos.d/
-	yum update -y ;
-	dnf install -y VirtualBox-5.0 ;
-	echo "export KERN_DIR=/usr/src/kernels/`uname -r`" >> /home/rtancman/.bashrc ;
-	service vboxdrv setup ;
+	dnf update -y
+	dnf install -y VirtualBox-5.1;
+	echo "export KERN_DIR=/usr/src/kernels/`uname -r`" >> /home/rtancman/.bashrc
+	/usr/lib/virtualbox/vboxdrv.sh setup
 
 	echo -e "${bakgrn}[Virtual Box 5.0] Finish Install ${txtrst}";
 
