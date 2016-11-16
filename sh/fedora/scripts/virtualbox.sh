1a3cb890e8ff1be03defc1feab45c43159235691
Raffael Tancman <raffael.tancman@m4u.com.br>
2016-11-16 16:44:10 -0200

    fix label fedora virtualbox

--- sh/fedora/scripts/virtualbox.sh
+++ sh/fedora/scripts/virtualbox.sh
@@ -4,11 +4,11 @@ VIRTUALBOX_IS_AVAILABLE="$(vboxmanage --version 2>&1 >/dev/null)"
 
 if [[ ${VIRTUALBOX_IS_AVAILABLE} == '' ]]; then
     
-    echo -e "${bakgrn}[installed][Virtual Box 5.0]${txtrst} already installed ;)" ;
+    echo -e "${bakgrn}[installed][Virtual Box 5.1]${txtrst} already installed ;)" ;
 	
 else
 
-	echo -e "${bakcyn}[Virtual Box 5.0] Start Install ${txtrst}";
+	echo -e "${bakcyn}[Virtual Box 5.1] Start Install ${txtrst}";
 
 	wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
 	mv virtualbox.repo /etc/yum.repos.d/
@@ -17,6 +17,6 @@ else
 	echo "export KERN_DIR=/usr/src/kernels/`uname -r`" >> /home/rtancman/.bashrc
 	/usr/lib/virtualbox/vboxdrv.sh setup
 
-	echo -e "${bakgrn}[Virtual Box 5.0] Finish Install ${txtrst}";
+	echo -e "${bakgrn}[Virtual Box 5.1] Finish Install ${txtrst}";
 
 fi
