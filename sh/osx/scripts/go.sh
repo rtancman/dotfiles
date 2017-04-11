65bf6618ef0999e8754c6383f6bd51129f3a9d44
Raffael.Tancman <raffael.tancman@corp.globo.com>
2017-04-11 10:34:59 -0300

    add go in osx

---
+++ sh/osx/scripts/go.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+GO_IS_AVAILABLE="$(sudo -u $DOTFILE_DEFAULT_USER go --version 2>&1 >/dev/null)"
+
+if [[ ${GO_IS_AVAILABLE} == '' ]]; then
+
+    echo -e "${bakgrn}[installed][go]${txtrst} already installed ;)" ;
+
+else
+
+    echo -e "${bakcyn}[go] Start Install ${txtrst}";
+
+    sudo -u $DOTFILE_DEFAULT_USER brew install go goenv
+
+    echo -e "${bakgrn}[go] Finish Install ${txtrst}";
+
+fi
