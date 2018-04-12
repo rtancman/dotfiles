60af16f70a16b50023d6add2582e6abd5fc4189f
Raffael Tancman <rtancman@gmail.com>
2018-04-12 14:41:31 -0300

    remove install errors packages in fedora

--- sh/common/python/main.sh
+++ sh/common/python/main.sh
@@ -2,13 +2,11 @@
 
 echo -e "${bakcyn}[common][python] Start Install ${txtrst}";
 
-. sh/common/python/pip/main.sh;
+# . sh/common/python/pip/main.sh;
 . sh/common/python/scripts/pyenv.sh;
 . sh/common/python/scripts/pyenv-virtualenv.sh;
 . sh/common/python/scripts/pyenv-virtualenvwrapper.sh;
 
-echo -e "${bakcyn} Remember ${txtrst}";
-echo -e "after install run $ bash ./dotfiles/sh/common/python/scripts/python-workspace_venv.sh
+echo -e "${bakcyn}[!!!][Remember] after install run $ bash ./dotfiles/sh/common/python/scripts/python-workspace_venv.sh ${txtrst}"
 
-"
 echo -e "${bakcyn}[common][python] Finish Install ${txtrst}";
