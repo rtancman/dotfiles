e22bd72dc4a81717a603e71b1e8ffcf3eb1af8e7
Raffael Tancman <rtancman@gmail.com>
2018-06-19 16:48:26 -0300

    fix nodejs fedora

--- sh/fedora/scripts/nodejs.sh
+++ sh/fedora/scripts/nodejs.sh
@@ -3,16 +3,10 @@
 NODEJS_IS_AVAILABLE="$(node -v 2>&1 >/dev/null)";
 
 if [[ ${NODEJS_IS_AVAILABLE} == '' ]]; then
-    
     echo -e "${bakgrn}[installed][NodeJS]${txtrst} already installed ;)" ;
-	
 else
-
 	echo -e "${bakcyn}[NodeJS] Start Install ${txtrst}";
-    
-    curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -;
-	dnf install -y nodejs;
-	
+    curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -
+    dnf install -y nodejs;
     echo -e "${bakgrn}[NodeJS] Finish Install ${txtrst}";
-
 fi
