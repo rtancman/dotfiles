4656b9342baa1b99382633125051dac444006b84
Raffael Tancman <rtancman@gmail.com>
2018-08-07 19:02:58 -0300

    add postman in flatpak fedora

--- sh/fedora/scripts/flatpak.sh
+++ sh/fedora/scripts/flatpak.sh
@@ -10,6 +10,7 @@ flatpak install -y flathub com.skype.Client
 flatpak install -y flathub com.jetbrains.PyCharm-Community
 flatpak install -y flathub com.jgraph.drawio.desktop
 flatpak install -y flathub com.obsproject.Studio
+flatpak install -y flathub com.getpostman.Postman
 
 echo -e "${bakgrn}[flatpak] Finish Install ${txtrst}";
 
