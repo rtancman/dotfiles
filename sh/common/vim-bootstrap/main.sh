631677291acf8764179056b66dc53e953a7b475b
Raffael Tancman <rtancman@gmail.com>
2022-12-12 11:56:59 -0300

    Add more languages in vimrc

--- sh/common/vim-bootstrap/main.sh
+++ sh/common/vim-bootstrap/main.sh
@@ -11,7 +11,7 @@ else
     # cp ./sh/common/vim-bootstrap/generate.vim /home/$DOTFILE_DEFAULT_USER/.vimrc;
     # chown $DOTFILE_DEFAULT_USER:$DOTFILE_DEFAULT_USER /home/$DOTFILE_DEFAULT_USER/.vimrc;
     # su - $DOTFILE_DEFAULT_USER -c "vim +PlugInstall +qall";
-    curl -X POST -d 'langs=c&langs=elixir&langs=elm&langs=erlang&langs=go&langs=html&langs=javascript&langs=php&langs=python&langs=ruby&editor=vim' https://vim-bootstrap.com/generate.vim >> /home/$DOTFILE_DEFAULT_USER/.vimrc
+    curl -X POST -d 'langs=c&langs=elixir&langs=elm&langs=erlang&langs=go&langs=html&langs=javascript&langs=php&langs=python&langs=ruby&langs=scala&langs=typescript&langs=rust&editor=vim' https://vim-bootstrap.com/generate.vim >> /home/$DOTFILE_DEFAULT_USER/.vimrc
     chown $DOTFILE_DEFAULT_USER:$DOTFILE_DEFAULT_USER /home/$DOTFILE_DEFAULT_USER/.vimrc;
 
     echo -e "${bakcyn}[common][vim-bootstrap] Finish Install ${txtrst}";
